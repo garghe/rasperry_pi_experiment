cea016f2d36468422b048870b349ffad2bb4511e
Marco Garghentini <garghe@gmail.com>
2018-06-01 16:27:12 +0100

    Playing around

--- test_sense_01.py
+++ test_sense_01.py
@@ -1,7 +1,16 @@
 from sense_hat import SenseHat
+import os
 
 sense = SenseHat()
 sense.clear()
 
-pressure = sense.get_pressure()
-print(pressure)
+temp = sense.get_temperature()
+cpu_temp = os.popen('/opt/vc/bin/vdgencmd measure_temp')
+print(str(cpu_temp))
+cputemp = cpu_temp.read()
+cputemp = cputemp.replace('temp=','')
+cputemp = cputemp.replace('\'C\n','')
+cputemp = float(cputemp)
+newtemp = temp - ((cputemp -temp) /2)
+
+print(newtemp)
